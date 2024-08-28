2a80608ae044bbce1a6e3c59607e4e97d8d74791
itznishantthapa <itsnishantu@gmail.com>
2024-08-28 14:43:57 +0545

    added decleration file

---
+++ dec.ts
@@ -0,0 +1,26 @@
+// declarations.d.ts
+declare module '*.png' {
+    const value: any;
+    export default value;
+  }
+  
+  declare module '*.jpg' {
+    const value: any;
+    export default value;
+  }
+  
+  declare module '*.jpeg' {
+    const value: any;
+    export default value;
+  }
+  
+  declare module '*.gif' {
+    const value: any;
+    export default value;
+  }
+  
+  declare module '*.svg' {
+    const content: any;
+    export default content;
+  }
+  
