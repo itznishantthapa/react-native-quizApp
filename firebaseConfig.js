38cb8b6bb2fe2f8bc81584b1a040c3ab6e729971
Nishant Thapa <160498680+itznishantthapa@users.noreply.github.com>
2024-09-02 15:05:19 +0545

    making things private

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,12 +4,7 @@ import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyByh9-ucAZmPmmYjsvMA94aL1BlXFIBPQk',
-  authDomain: 'react-native-quiz-v2.firebaseapp.com',
-  projectId: 'react-native-quiz-v2',
-  storageBucket: 'react-native-quiz-v2.appspot.com',
-  messagingSenderId: '1058251948585',
-  appId: '1:1058251948585:android:d001e79370319173c81238',
+ // privacy mantained by Nishant Thapa's whatsApp
 };
 
 const app = initializeApp(firebaseConfig);
