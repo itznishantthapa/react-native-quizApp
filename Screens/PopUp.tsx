8cb68e9315dcfa7e943a2cecb33b9e688b62a122
itznishantthapa <itsnishantu@gmail.com>
2024-09-23 22:43:01 +0545

    removed unnecessary imports

--- Screens/PopUp.tsx
+++ Screens/PopUp.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, Modal, ImageBackground, TouchableWithoutFeedback } from 'react-native';
+import { View, Text, TouchableOpacity, Modal, TouchableWithoutFeedback } from 'react-native';
 import React, { useState } from 'react';
 import { styles } from '../style/style';
 
